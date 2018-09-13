e90cdab68dda244260fef750502c491680cfeb96
Serega <sdfomin@edu.hse.ru>
2018-09-13 11:20:47 +0300

    Ready Task05

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -10,6 +10,21 @@ namespace Task05
     {
         static void Main(string[] args)
         {
+            double a, b;
+            string inputStr;
+            do
+            {
+                Console.Write("Введите длину первого катета: ");
+                inputStr = Console.ReadLine();
+            } while (!double.TryParse(inputStr, out a) || a <= 0);
+            do
+            {
+                Console.Write("Введите длину второго катета: ");
+                inputStr = Console.ReadLine();
+            } while (!double.TryParse(inputStr, out b) || b <= 0);
+            double ans = Math.Sqrt(a * a + b * b);
+            Console.WriteLine($"Гипотенуза: {ans}");
+            Console.ReadKey();
         }
     }
 }
